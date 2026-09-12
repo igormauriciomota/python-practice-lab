db009f453c992581ba20099d24aa5712c92e1088
igormauriciomota <107084301+igormauriciomota@users.noreply.github.com>
2026-09-12 17:49:40 -0300

    sequencia de Fibonacci

--- 06-python-function-practice/funcoes-com-parametros/02_Exercicio_01_fibonacci.py
+++ 06-python-function-practice/funcoes-com-parametros/02_Exercicio_01_fibonacci.py
@@ -2,21 +2,7 @@
 1 - Criar uma funcao recursiva (que retorne ela mesma) para armazenar N termos dea sequencia de Fibonacci
 em uma lista. N é definido pelo usuario. Ao encontrar os termos, imprimir a lista e finalizar a função.
 
-2 - Criar 5 funçoes: uma para um cadastro, outra para realizar o login, outra para mudar a senha,
-outra para realizar logout e ainda uma para definir qual opção o usuario deseja escolher.
-Utilize um loop while para sari do sistema apenas se o usuario desejar (Criar a opsão 'sair')
-Atente-se as regras:
-
- - Só é possivel realizar um cadastro se não houver nehum anterior.
- - Só é possivel realizar login se houver um cadastro. 
- - Só é possivel realizar login se usuario informar corretamente username e senha.
- - Só é possivel realizar a senha se o usuario estiver logado.
- - Só é possivel realizar a senha se o usuario informar corretamente a senha atual.
- - Só é possivel realizar logout se o usuario estiver logado
- 
  """
-
-# 1
 
 listaSF = []
 stop = 0
---
+++ 06-python-function-practice/funcoes-com-parametros/03_Exercicio_02_cadastro.py
@@ -0,0 +1,20 @@
+"""
+
+2 - Criar 5 funçoes: uma para um cadastro, outra para realizar o login, outra para mudar a senha,
+outra para realizar logout e ainda uma para definir qual opção o usuario deseja escolher.
+Utilize um loop while para sari do sistema apenas se o usuario desejar (Criar a opsão 'sair')
+Atente-se as regras:
+
+ - Só é possivel realizar um cadastro se não houver nehum anterior.
+ - Só é possivel realizar login se houver um cadastro. 
+ - Só é possivel realizar login se usuario informar corretamente username e senha.
+ - Só é possivel realizar a senha se o usuario estiver logado.
+ - Só é possivel realizar a senha se o usuario informar corretamente a senha atual.
+ - Só é possivel realizar logout se o usuario estiver logado
+
+"""
+
+
+
+
+
